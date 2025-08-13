07df1ca37d50f0809dc1f7e33a0992e771e76346
mike <mixailted@gmail.com>
2025-08-13 23:49:40 +0400

    Refactor: test logic

--- conftest.py
+++ conftest.py
@@ -1,8 +1,13 @@
 import pytest
 from  selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
 @pytest.fixture()
 def browser():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('window-size=1920x1080')
+    driver = webdriver.Chrome(options=options)
     driver.get('https://store.steampowered.com/')
     yield driver
     driver.quit()
--- test_steam.py
+++ test_steam.py
@@ -6,24 +6,23 @@ import faker
 
 faker = faker.Faker()
 CURRENT_URL = 'https://store.steampowered.com/'
-MAIN_TITLE = By.XPATH,"//*[contains(@id, 'logo_holder')]//img[contains(@alt, 'Steam')]"
-LOGIN_URL = 'https://store.steampowered.com/login/?redir=&redir_ssl=1'
+MAIN_TITLE = By.XPATH,"//*[@id='home_featured_and_recommended']"
+WAIT = 10
+
 SIGN_IN = By.XPATH, "(//*[contains(text(), 'login')])[1]"
 INPUT_MAIL = By.XPATH, "(//input[contains(@type, 'text')])[1]"
 INPUT_PASS = By.XPATH, "//input[contains(@type, 'password')]"
 BTN_SIGN_IN = By.XPATH, "//button[contains(text(), 'Sign in')]"
-ERR_MSG = By.XPATH, "//*[@data-featuretarget='login']//div[5]"
+ERR_MSG = By.XPATH, "//*[contains(text(), 'password and account')]"
 
 
 def test_steam(browser: WebDriver):
-    browser.set_window_size(1920, 1080)
     assert browser.current_url == CURRENT_URL
     assert browser.execute_script('return document.readyState;') == 'complete'
-    wait = WebDriverWait(browser, 10)
-    assert wait.until(EC.presence_of_element_located(MAIN_TITLE))
+    wait = WebDriverWait(browser, WAIT)
+    wait.until(EC.presence_of_element_located(MAIN_TITLE))
     sign_in = wait.until(EC.element_to_be_clickable(SIGN_IN))
     sign_in.click()
-    assert wait.until(EC.url_to_be(LOGIN_URL))
     assert browser.execute_script('return document.readyState;') == 'complete'
     input_mail = wait.until(EC.element_to_be_clickable(INPUT_MAIL))
     input_mail.clear()
@@ -35,4 +34,4 @@ def test_steam(browser: WebDriver):
     input_pass.send_keys(faker.password())
     btn_sign = wait.until(EC.element_to_be_clickable(BTN_SIGN_IN))
     btn_sign.click()
-    wait.until(EC.visibility_of_element_located(ERR_MSG))
+    assert wait.until(EC.text_to_be_present_in_element(ERR_MSG, 'password and account'))
